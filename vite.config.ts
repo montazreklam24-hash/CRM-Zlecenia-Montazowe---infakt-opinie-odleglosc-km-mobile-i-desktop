1bb6a6ad66f8075f2586e6a14b68c28ac85fecde
MontazReklam24 <montazreklam24@gmail.com>
2025-12-23 19:35:50 +0100

    fix: aktualizacja proxy backendu na 192.168.1.50:8080

--- vite.config.ts
+++ vite.config.ts
@@ -24,14 +24,15 @@ export default defineConfig({
     }
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.1.50:8080',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.1.50:8080',
         changeOrigin: true,
       }
     }
